b504f4ba2f04152ce3905fe1ae8ed3cc4a3273e9
SabinoOrbello <gemellus95@outlook.it>
2024-02-02 14:14:41 +0100

    Aggiungere i file di progetto.

---
+++ AgenziaEntrate/Contribuente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenziaEntrate
+{
+    internal class Contribuente
+    {
+        // Proprietà della classe
+        public string Nome {  get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataDiNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneDiResidenza { get; set; }
+        public decimal RedditoAnnuale { get; set; }
+
+        //Costruttore della classe
+        public Contribuente(string nome, string cognome, DateTime dataDiNascita, string codiceFiscale, char sesso, string comuneDiResidenza, decimal redditoAnnuale)
+        {
+            // Inizializzazione delle proprietà con i valori passati al costruttore
+            Nome = nome;
+            Cognome = cognome;
+            DataDiNascita = dataDiNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneDiResidenza = comuneDiResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        // metodo per calcolare l'imposta da versare
+        public decimal CalcolaImposta()
+        {
+            // variabile che memorizza l'imposta
+            decimal imposta = 0;
+            
+            // Calcolo dell'imposta in base al reddito inserito
+            if(RedditoAnnuale <= 15000)
+            {
+                imposta = RedditoAnnuale * 0.23m;
+            }
+            else if(RedditoAnnuale <= 28000)
+            {
+                imposta = 3450 + (RedditoAnnuale - 15000) * 0.27m;
+            }
+            else if(RedditoAnnuale <= 55000)
+            {
+                imposta = 6960 + (RedditoAnnuale - 28000) * 0.38m;
+            }
+            else if(RedditoAnnuale <= 75000)
+            {
+                imposta = 17220 + (RedditoAnnuale - 55000) * 0.41m;
+            }
+            else
+            {
+                imposta = 25420 + (RedditoAnnuale - 75000) * 0.43m;
+            }
+            // Restituisce l'imposta calcolata
+            return imposta;
+        }
+    }
+}
---
+++ AgenziaEntrate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenziaEntrate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci i dati del contribuente: ");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Cognome: ");
+            string cognome = Console.ReadLine();
+
+            Console.Write("Data di nascita (formato gg/mm/aaaa): ");
+            DateTime dataNascita = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null); // quì convertiamo la stringa di una data in un oggetto DateTime
+
+            Console.Write("Codice fiscale: ");
+            string codiceFiscale = Console.ReadLine();
+
+            Console.WriteLine("Sesso (M/F): ");
+            char sesso = Console.ReadLine().ToUpper()[0]; // toUpper ci assicura che il carattere inserito sia convertito in maiuscolo
+
+            Console.Write("Comune di residenza: ");
+            string comuneResidenza = Console.ReadLine();
+
+            Console.Write("Reddito Annuale: ");
+            decimal redditoAnnuale = decimal.Parse(Console.ReadLine()); // convertiamo la stringa ottenuta in un tipo decimal
+
+            // creazione dell'oggetto Contribuente
+            Contribuente contribuente1 = new Contribuente(nome, cognome, dataNascita, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+
+             // calcolo dell'imposta da versare
+            decimal impostaDaVersare = contribuente1.CalcolaImposta();
+
+            Console.WriteLine("\nRISULTATO RIEPILOGATIVO:");
+            Console.WriteLine($"Contribuente: {contribuente1.Nome} {contribuente1.Cognome},");
+            Console.WriteLine($"nato il {contribuente1.DataDiNascita.ToString("dd/MM/yyyy")} ({contribuente1.Sesso}),");
+            Console.WriteLine($"residente in {contribuente1.ComuneDiResidenza},");
+            Console.WriteLine($"codice fiscale: {contribuente1.CodiceFiscale}");
+            Console.WriteLine($"Reddito dichiarato: $ {contribuente1.RedditoAnnuale} ");
+            Console.WriteLine($"IMPOSTA DA VERSARE: $ {impostaDaVersare} ");
+
+
+            Console.ReadLine();
+        }
+    }
+}
